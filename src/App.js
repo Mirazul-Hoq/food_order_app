f5778ca21785a9cfa5d7dda43a71fa8097adf4d3
Mirazul-Hoq <mirazulhoq807@gmail.com>
2022-02-17 17:03:39 +0600

    Food cart added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+
+  const [cartIsShown, setCartisShown] = useState();
+
+  const showCartHandler = () => { setCartisShown(true) };
+  const hideCartHandler = () => { setCartisShown(false) };
+
   return (
     <Fragment>
-      <Header/>
+      { cartIsShown && <Cart onCloseCart={hideCartHandler} /> }
+      <Header onShowCart = { showCartHandler }/>
       <main>
         <Meals />
       </main>
